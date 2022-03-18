ef6cc0f0771836595a3d791ae803dbb7e3ff7665
Leonardo Almeida de Araújo <leonardo.a@acad.ifma.edu.br>
2022-03-18 18:41:25 -0300

    Changing routes and GitHub Api

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
-import express from "express";
+import "dotenv/config"
+import express, { response } from "express";
 
 const app = express();
 var port = 4040
+
+app.get("/github", (request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`)
+});
 
 app.listen(port, () => console.log("Running on port:", port));
